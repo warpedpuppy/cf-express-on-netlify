e80d50026b7d360dfffdcbc15e947116023a1c05
ted <ted@warpedpuppy.com>
2023-01-21 10:04:22 -0800

    create con function

--- server-local.js
+++ server-local.js
@@ -1,5 +1,6 @@
-const app = require('./express/server');
 
+const app = require('./express/server');
+const mongoose = require('mongoose');
 const { LOCAL_PORT } = require('./config')
 
 app.listen(LOCAL_PORT, () => console.log(`Local app listening on port ${LOCAL_PORT}!`));
