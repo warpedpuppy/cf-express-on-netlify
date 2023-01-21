48a5a465bd6afd15215ab5c10b95e2742fa37c9e
ted <ted@warpedpuppy.com>
2023-01-21 07:06:06 -0800

    removed connection for testing

--- express/server.js
+++ express/server.js
@@ -4,14 +4,14 @@ const app = express();
 app.use(express.json());
 const { API_ROOT, CONNECTION_URI } = require('../config');
 
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
-mongoose.connect(CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then( () => console.log('connected'))
-.catch( e => console.error(e) );
+// const mongoose = require("mongoose");
+// mongoose.set('strictQuery', true);
+// mongoose.connect(CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then( () => console.log('connected'))
+// .catch( e => console.error(e) );
 
 
 const API_ROUTER = express.Router();
