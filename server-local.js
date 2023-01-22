db00c7d8d1739d63c99c04aab062b65d7baf9ac0
ted <ted@warpedpuppy.com>
2023-01-22 08:13:56 -0800

    connect

--- server-local.js
+++ server-local.js
@@ -1,14 +1,23 @@
-const app = require('./express/server');
-const mongoose = require('mongoose');
 
-const { CONNECTION_URI } = require('./config');
-mongoose.connect(CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then( () => {console.log('connected'); app.set('db', "connected")})
-.catch( e => console.error(e) );
+// const mongoose = require('mongoose');
 
-const { LOCAL_PORT } = require('./config');
+// const { CONNECTION_URI } = require('./config');
+// mongoose.connect(CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then( () => {console.log('connected'); app.set('db', "connected")})
+// .catch( e => console.error(e) );
+async function c() {
+	let connect = await require('./express/mongoose-connect');
+	let conn = await connect.connect();
+	console.log(conn)
+	const app = await require('./express/server');
+	app.set('db', "connected");
+	const { LOCAL_PORT } = require('./config');
 
-app.listen(LOCAL_PORT, () => console.log(`Local app listening on port ${LOCAL_PORT}!`));
+	app.listen(LOCAL_PORT, () => console.log(`Local app listening on port ${LOCAL_PORT}!`));
+}
+c();
+
+
