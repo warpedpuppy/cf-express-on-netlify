62ab8ba7de831ab075db1d95f4c84df41e13e30b
ted <ted@warpedpuppy.com>
2023-01-22 08:18:25 -0800

    connect

--- express/mongoose-connect.js
+++ express/mongoose-connect.js
@@ -1,22 +1,22 @@
-'use strict';
+// 'use strict';
 
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-const { CONNECTION_URI } = require('../config');
-let conn = null;
+// const mongoose = require('mongoose');
+// mongoose.set('strictQuery', true);
+// const { CONNECTION_URI } = require('../config');
+// let conn = null;
 
-const uri = CONNECTION_URI;
+// const uri = CONNECTION_URI;
 
-exports.connect = async function() {
-  if (conn == null) {
-    conn = mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000
-    }).then(() => mongoose);
-    // `await`ing connection after assigning to the `conn` variable
-    // to avoid multiple function calls creating new connections
-    await conn;
-  }
+// exports.connect = async function() {
+//   if (conn == null) {
+//     conn = mongoose.connect(uri, {
+//       serverSelectionTimeoutMS: 5000
+//     }).then(() => mongoose);
+//     // `await`ing connection after assigning to the `conn` variable
+//     // to avoid multiple function calls creating new connections
+//     await conn;
+//   }
 
-  return conn;
-};
+//   return conn;
+// };
 
--- server-local.js
+++ server-local.js
@@ -1,23 +1,36 @@
 
-// const mongoose = require('mongoose');
 
-// const { CONNECTION_URI } = require('./config');
-// mongoose.connect(CONNECTION_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then( () => {console.log('connected'); app.set('db', "connected")})
-// .catch( e => console.error(e) );
+
+// async function c() {
+// 	let connect = await require('./express/mongoose-connect');
+// 	let conn = await connect.connect();
+// 	console.log(conn)
+// 	const app = await require('./express/server');
+// 	app.set('db', "connected");
+// 	const { LOCAL_PORT } = require('./config');
+
+// 	app.listen(LOCAL_PORT, () => console.log(`Local app listening on port ${LOCAL_PORT}!`));
+// }
+// c();
+
+
 async function c() {
-	let connect = await require('./express/mongoose-connect');
-	let conn = await connect.connect();
-	console.log(conn)
+	
+	const mongoose = require('mongoose');
+
+	const { CONNECTION_URI } = require('./config');
+	let conn = await mongoose.connect(CONNECTION_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	})
+
+
+
 	const app = await require('./express/server');
-	app.set('db', "connected");
+	if (conn) {app.set('db', "connected") } else { app.set('db', " not connected") };
 	const { LOCAL_PORT } = require('./config');
 
 	app.listen(LOCAL_PORT, () => console.log(`Local app listening on port ${LOCAL_PORT}!`));
 }
 c();
-
 
